7c47ce772d8013a928f5d1a8733e7c79ef7041e1
Luc <luc.lebosse@gmail.com>
2015-02-20 16:56:04 +0800

    Sync with repetier 0.91/0.92
    Was set to 0 when repetier set it to 1, no idea why this was set to 0,
    did several print and did not see any drawback so put 1

--- src/ArduinoDUE/Repetier/Configuration.h
+++ src/ArduinoDUE/Repetier/Configuration.h
@@ -486,7 +486,7 @@ If your EXT0_PID_MAX is low, you should prefer the second method.
 */
 #define SCALE_PID_TO_MAX 0
 
-#define HEATER_PWM_SPEED 0 // How fast ist pwm signal 0 = 15.25Hz, 1 = 30.51Hz, 2 = 61.03Hz, 3 = 122.06Hz
+#define HEATER_PWM_SPEED 1 // How fast ist pwm signal 0 = 15.25Hz, 1 = 30.51Hz, 2 = 61.03Hz, 3 = 122.06Hz
 
 /** Temperature range for target temperature to hold in M109 command. 5 means +/-5 degC
 
