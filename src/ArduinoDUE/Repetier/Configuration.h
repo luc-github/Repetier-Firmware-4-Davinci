e12d09805b12289249b4692eb3c21aec9f1f775f
Luc <luc.lebosse@gmail.com>
2015-03-19 10:53:58 +0800

    Change version as several changes due to 0.92.3
    The import and code cleaning deserve a version change to trigger
    but several changes will come to fix import issues

--- src/ArduinoDUE/Repetier/Configuration.h
+++ src/ArduinoDUE/Repetier/Configuration.h
@@ -32,7 +32,7 @@
 #define VERSION_MAJOR " 1"
 #define VERSION_MINOR_YEAR "15"
 #define VERSION_MINOR_MONTH "03"
-#define VERSION_MINOR_DAY "02"
+#define VERSION_MINOR_DAY "19"
 #define VERSION_BUILD "1"
 
 //Davinci screen is not standard reprap it is WINSTAR 16x4
