8c663b71e9f4734ff3e3e88f90d000fd070d88dc
luc <luc.lebosse@tech-hunters.com>
2016-04-17 09:23:33 +0200

    test davinci 1 and 2 script

--- buildscript.sh
+++ buildscript.sh
@@ -7,9 +7,23 @@ arduino --board arduino:sam:arduino_due_x --save-prefs
 arduino --get-pref sketchbook.path
 
 if [ "$DAVINCI" = "1M0" ]; then
-	echo "Davinci Model 0"
+	echo "Davinci 1 Model 0"
+	sed -i 's/#define DAVINCI 0 /#define DAVINCI 1 /g' ./src/ArduinoDUE/Repetier/Configuration.h
+	sed -i 's/#define DAVINCI 2 /#define DAVINCI 1 /g' ./src/ArduinoDUE/Repetier/Configuration.h
+	sed -i 's/#define DAVINCI 3 /#define DAVINCI 1 /g' ./src/ArduinoDUE/Repetier/Configuration.h
+	sed -i 's/#define DAVINCI 4 /#define DAVINCI 1 /g' ./src/ArduinoDUE/Repetier/Configuration.h
+	sed -i 's/#define MODEL  1/#define MODEL  0/g' ./src/ArduinoDUE/Repetier/Configuration.h
+else
+	if [ "$DAVINCI" = "2M0" ]; then
+	echo "Davinci 2 Model 0"
+	sed -i 's/#define DAVINCI 0 /#define DAVINCI 2 /g' ./src/ArduinoDUE/Repetier/Configuration.h
+	sed -i 's/#define DAVINCI 1 /#define DAVINCI 2 /g' ./src/ArduinoDUE/Repetier/Configuration.h
+	sed -i 's/#define DAVINCI 3 /#define DAVINCI 2 /g' ./src/ArduinoDUE/Repetier/Configuration.h
+	sed -i 's/#define DAVINCI 4 /#define DAVINCI 2 /g' ./src/ArduinoDUE/Repetier/Configuration.h
+	sed -i 's/#define MODEL  1/#define MODEL  0/g' ./src/ArduinoDUE/Repetier/Configuration.h
 else
 	echo "Unknown model: $DAVINCI"
+if
 if
 
 build_sketch ./src/ArduinoDUE/Repetier/Repetier.ino
