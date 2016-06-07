7cacfb5f0548d62b00b122e85178d72b11b3099c
luc <luc.lebosse@tech-hunters.com>
2016-06-07 08:52:01 +0200

    Update version

--- src/ArduinoDUE/Repetier/Configuration.h
+++ src/ArduinoDUE/Repetier/Configuration.h
@@ -32,7 +32,7 @@
 #define VERSION_MAJOR "1"
 #define VERSION_MINOR_YEAR "16"
 #define VERSION_MINOR_MONTH "06"
-#define VERSION_MINOR_DAY "05"
+#define VERSION_MINOR_DAY "07"
 #define VERSION_BUILD "1"
 
 //Davinci screen is not standard reprap it is WINSTAR 16x4
